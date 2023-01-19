fd9f4243cc9f5540c0a8254eaed39db0e6973d9a
Tong030905 <1463921624@qq.com>
2023-01-19 13:59:35 +0800

    feat(*): UI-Menu Student，Teacher页面  更新

---
+++ src/pages/Student.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+  import Top from '../components/Top.vue';
+  import StudentMenu from '../components/StudentMenu.vue';
+</script>
+
+<template>
+ <StudentMenu></StudentMenu>
+ <Top></Top>
+</template>
+
+
+
--- src/routers/index.ts
+++ src/routers/index.ts
@@ -3,7 +3,13 @@ import Template from "../components/Template.vue";
 import Change from "../components/Change.vue";
 import Complaints from "../components/Complaints.vue";
 import Query from "../components/Query.vue"
-import Layout from "../pages/Layout.vue";
+import Student from "../pages/Student.vue";
+import Teacher from "../pages/Teacher.vue";
+import ChatGround from "../components/ChatGround.vue";
+import MailBox from "../components/MailBox.vue";
+import Examine from "../components/Examine.vue";
+import Manage from "../components/Manage.vue";
+
 import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
@@ -11,43 +17,105 @@ const routes = [
     path: "/",
     redirect: "/Login"
   },
+  //默认路由为登陆界面
   {
     path: "/Login",
     name: "Login",
     component: Login
   },
+  //登录界面
   {
-    path: "/Layout",
-    name: "Layout",
+    path: "/Student",
+    name: "Layout1",
     components: {
-      'default': Layout,
+      'default': Student,
       'right': Template,
     }
   },
   {
-    path: "/Layout",
-    name: "Change",
+    path: "/Student",
+    name: "Change1",
     components: {
-      'default': Layout,
+      'default': Student,
       'right': Change,
     }
   },
   {
-    path: "/Layout",
+    path: "/Student",
     name: "Complaints",
     components: {
-      'default': Layout,
+      'default': Student,
       'right': Complaints,
     }
   },
   {
-    path: "/Layout",
+    path: "/Student",
     name: "Query",
     components: {
-      'default': Layout,
+      'default': Student,
       'right': Query,
     }
   },
+  {
+    path: "/Student",
+    name: "ChatGround1",
+    components: {
+      'default': Student,
+      'right':ChatGround,
+    }
+  },                                        
+  {
+    path: "/Student",
+    name: "MailBox",
+    components: {
+      'default': Student,
+      'right':MailBox,
+    }
+  },                                        
+  
+  //学生用户界面
+
+  {
+    path: "/Teacher",
+    name: "Layout2",
+    components: {
+      'default': Teacher,
+      'right': Template,
+    }
+  },
+  {
+    path: "/Teacher",
+    name: "Change2",
+    components: {
+      'default': Teacher,
+      'right': Change,
+    }
+  },
+  {
+    path: "/Teacher",
+    name: "Examine",
+    components: {
+      'default': Teacher,
+      'right': Examine,
+    }
+  },
+  {
+    path: "/Teacher",
+    name: "ChatGround2",
+    components: {
+      'default': Teacher,
+      'right': ChatGround,
+    }
+  },
+  {
+    path: "/Teacher",
+    name: "Manage",
+    components: {
+      'default': Teacher,
+      'right': Manage,
+    }
+  }
+  //辅导员登陆界面
 ]
 
 const router = createRouter({

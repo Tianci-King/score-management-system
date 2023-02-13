0ecce28d7d815afbc5a35e8f0472de1810e6586f
Patrickill <1322301665@qq.com>
2023-02-13 19:35:37 +0800

    vite配置baseURL

--- src/apis/utils/request.ts
+++ src/apis/utils/request.ts
@@ -3,7 +3,6 @@ import axios from "axios";
 const request = (config: any) => {
     const serve = axios.create({
         timeout: 1000,
-        baseURL: "http://127.0.0.1:4523/m1/2202253-0-default",
     })
     return serve(config);
 };
